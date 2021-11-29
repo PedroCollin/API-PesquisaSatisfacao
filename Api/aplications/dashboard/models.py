84ce9820e7c2403af5d2f21f6f99135e19f20d9b
Pedro Collin <pedrocollintkd@gmail.com>
2021-11-29 11:36:04 -0300

    29.11

--- Api/aplications/dashboard/models.py
+++ Api/aplications/dashboard/models.py
@@ -5,7 +5,7 @@ from ..formulario.models import Formulario
 class Dashboard(models.Model):
 
     # formularios_preenchidos = models.objects.filter(id_pergunta=1)
-    # qtd_frm_preenchidos = models.IntegerField(default=0)
+    qtd_frm_preenchidos = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s' % self.numero
+        return str(Dashboard.objects.all().count())
